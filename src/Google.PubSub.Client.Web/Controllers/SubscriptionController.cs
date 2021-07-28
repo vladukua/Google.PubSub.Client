4c3f386367fd75b58ae364c6fd2a94892035bace
Volodymyr Teodorovych <vladukteo@gmail.com>
2021-07-28 13:12:29 +0300

    Added an ability to create subscription with retry policy.

--- src/Google.PubSub.Client.Web/Controllers/SubscriptionController.cs
+++ src/Google.PubSub.Client.Web/Controllers/SubscriptionController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Google.Cloud.PubSub.V1;
+using Google.Protobuf.WellKnownTypes;
 using Google.PubSub.Client.Web.Configs;
 using Grpc.Core;
 using Microsoft.AspNetCore.Mvc;
@@ -39,7 +40,13 @@ namespace Google.PubSub.Client.Web.Controllers
         }
 
         [HttpPost("project/subscription/create")]
-        public async Task<ActionResult> Create(string projectId, string topicName, string subscriptionId,
+        public async Task<ActionResult> Create(
+            string projectId, 
+            string topicName, 
+            string subscriptionId,
+            string retryPolicy,
+            int minBackoff,
+            int maxBackoff,
             CancellationToken cancellationToken)
         {
             var client = await new SubscriberServiceApiClientBuilder
@@ -48,7 +55,7 @@ namespace Google.PubSub.Client.Web.Controllers
                 ChannelCredentials = ChannelCredentials.Insecure
             }.BuildAsync(cancellationToken);
 
-            await CreateSubscriptionAsync(client, subscriptionId, topicName, projectId, cancellationToken);
+            await CreateSubscriptionAsync(client, subscriptionId, topicName, projectId, retryPolicy, minBackoff, maxBackoff, cancellationToken);
 
             return RedirectToAction("Detail", "Topic", new {projectId, topicName});
         }
@@ -86,9 +93,20 @@ namespace Google.PubSub.Client.Web.Controllers
 
             var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
 
+            var subscription = await client.GetSubscriptionAsync(subscriptionName, cancellationToken);
+            model.Details = new
+            {
+                subscription.RetryPolicy,
+                subscription.ExpirationPolicy,
+                subscription.AckDeadlineSeconds,
+                subscription.DeadLetterPolicy,
+                subscription.RetainAckedMessages,
+                subscription.EnableMessageOrdering,
+            };
+
             try
             {
-                var pullResponse = await client.PullAsync(subscriptionName, false, int.MaxValue, cts.Token);
+                var pullResponse = await client.PullAsync(subscriptionName, true, int.MaxValue, cts.Token);
                 model.Messages = pullResponse.ReceivedMessages.Select(m =>
                 {
                     var dataStr = m.Message.Data.ToStringUtf8();
@@ -142,6 +160,7 @@ namespace Google.PubSub.Client.Web.Controllers
                 ChannelCredentials = ChannelCredentials.Insecure
             }.BuildAsync(cancellationToken);
             var subscriptionId = SubscriptionName.Parse(subscriptionName).SubscriptionId;
+            var subscription = await client.GetSubscriptionAsync(subscriptionId, cancellationToken);
 
             await DeleteSubscriptionAsync(client, subscriptionName, cancellationToken);
             await CreateSubscriptionAsync(
@@ -149,6 +168,9 @@ namespace Google.PubSub.Client.Web.Controllers
                 subscriptionId,
                 topicName,
                 projectId,
+                subscription.RetryPolicy == null ? "immediate" : "exponentialBackoff",
+                (subscription.RetryPolicy?.MinimumBackoff.Seconds).GetValueOrDefault(),  
+                (subscription.RetryPolicy?.MaximumBackoff.Seconds).GetValueOrDefault(),
                 cancellationToken);
 
             return RedirectToAction("Detail", "Topic", new {projectId, topicName});
@@ -158,12 +180,29 @@ namespace Google.PubSub.Client.Web.Controllers
             string subscriptionId,
             string topicName,
             string projectId,
+            string retryPolicy,
+            long minBackoff,
+            long maxBackoff,
             CancellationToken cancellationToken)
         {
             var name = SubscriptionName.FromProjectSubscription(projectId, subscriptionId);
             var topic = TopicName.Parse(topicName);
+
+            var subscriptionRequest = new Subscription
+            {
+                SubscriptionName = name,
+                TopicAsTopicName = topic,
+                AckDeadlineSeconds = 30,
+                RetryPolicy = retryPolicy == "exponentialBackoff"
+                    ? new RetryPolicy
+                    {
+                        MinimumBackoff = Duration.FromTimeSpan(TimeSpan.FromSeconds(minBackoff)),
+                        MaximumBackoff = Duration.FromTimeSpan(TimeSpan.FromSeconds(maxBackoff)),
+                    }
+                    : null,
+            };
             var subscription =
-                await client.CreateSubscriptionAsync(name, topic, new PushConfig(), 10, cancellationToken);
+                await client.CreateSubscriptionAsync(subscriptionRequest, cancellationToken);
 
             return subscription;
         }
@@ -184,6 +223,8 @@ namespace Google.PubSub.Client.Web.Controllers
         public string SubscriptionName { get; set; }
 
         public IReadOnlyCollection<Message> Messages { get; set; } = Array.Empty<Message>();
+
+        public object Details { get; set; }
 
         public class Message
         {
